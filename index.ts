69ec0332ffc88dcea826e5ccdff2b2af785dcb8e
EdwinMLara <emlara35@gmail.com>
2023-12-12 11:07:38 -0600

    chore: :rocket: configuraciones para libreria

---
+++ index.ts
@@ -0,0 +1,8 @@
+
+import {default as List} from "./src/list";
+import Queue from "./src/queque";
+import Stack from "./src/Stack";
+import Head from "./src/Head";
+import BTree from "./src/binaryTree";
+
+export {List,Queue,Stack,Head,BTree}

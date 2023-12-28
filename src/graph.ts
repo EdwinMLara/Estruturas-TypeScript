c01037769d1fc05b2bb4e63652e9a28c21bfbd16
EdwinMLara <emlara35@gmail.com>
2023-12-28 12:05:47 -0600

    feat: :art: se agregaron los metodos de añadir vertices y bordes
    este tipo de grafo esta basado en listas de adyacencia

--- src/graph.ts
+++ src/graph.ts
@@ -1,4 +1,5 @@
 import LinkedList from "./list";
+import { equals } from "./utils";
 
 class NodeG<Type>{
     value: Type;
@@ -16,4 +17,35 @@ class GraphLA<Type>{
     constructor(value : Type){
         this.vertices = new LinkedList(new NodeG(value));
     }
+
+    addEdges(origin : Type, value : Type) : boolean {
+        if(typeof(value) === 'object'){
+            if(!equals(origin, value))
+                return false;
+        }
+        console.log("no es object");
+        let auxVertices = this.vertices;
+        let auxVerticesList = auxVertices.head
+            while(auxVerticesList !== null){
+                console.log(auxVerticesList.data.value)
+                if(auxVerticesList.data.value === origin){
+                    console.log(auxVerticesList.data, origin);
+                    if(auxVerticesList.data.listaAdyacencua === null){
+                        auxVerticesList.data.listaAdyacencua = new LinkedList(value);
+                        return true;        
+                    }
+                    auxVerticesList.data.listaAdyacencua.insertLast(value);
+                    return true;
+                }
+                auxVerticesList = auxVerticesList.next; 
+            }
+        return true;
+    }
+
+    addVertice(value: Type): void{
+        let auxNodeG =  new NodeG(value)
+        this.vertices.insertLast(auxNodeG);
+    }
 }
+
+export default GraphLA;

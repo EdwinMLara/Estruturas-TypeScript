dd10f0e21fa610322f866c006e472eaf19cc7424
EdwinMLara <emlara35@gmail.com>
2024-04-10 11:15:43 -0600

    feat: :art: se agrego el metodo de imprimir lista
    funciona para ver los datos presentes en la lista

--- src/queque.ts
+++ src/queque.ts
@@ -1,35 +1,43 @@
-import LinkedList from './list'
+import LinkedList from "./list";
 
-class Queue<Type>{
-    headQ : LinkedList<Type>;
+class Queue<Type> {
+  headQ: LinkedList<Type>;
 
-    constructor(value : Type){
-        this.headQ = new LinkedList(value);
+  constructor(value: Type) {
+    this.headQ = new LinkedList(value);
+  }
+
+  isEmpty(): boolean {
+    if (this.headQ.head === null) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    isEmpty():boolean{
-        if(this.headQ.head === null){
-            return true;
-        }else{
-            return false
-        }
-    }
+  size() {
+    return this.headQ.size();
+  }
 
-    size(){
-        return this.headQ.size();
-    }
+  enqueue(value: Type) {
+    this.headQ.insertLast(value);
+  }
 
-    enqueue(value : Type){
-        this.headQ.insertLast(value);
-    }
+  dequeue() {
+    return this.headQ.deleteFirst();
+  }
 
-    dequeue(){
-        return this.headQ.deleteFirst();
-    }
+  peak() {
+    return this.headQ.head.data;
+  }
 
-    peak(){
-        return this.headQ.head.data;
+  printQueue() {
+    let aux = this.headQ.head;
+    while (aux != null) {
+      console.log(aux.data);
+      aux = aux.next;
     }
+  }
 }
 
 export default Queue;

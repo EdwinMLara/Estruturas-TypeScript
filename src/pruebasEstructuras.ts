e730a9e95fd9feea5757e91cf0a1f9f6fa04bff8
meli espinosa <meliespinosa490>
2025-01-13 14:19:31 -0600

    test: :bug: Testing the Breadth and Depth Search Tranversal
    To make sure the result is succeful we use an example that it can be found in the graph section of the page w3shool

--- src/pruebasEstructuras.ts
+++ src/pruebasEstructuras.ts
@@ -1,13 +1,31 @@
 import GraphLA from "./graph";
 
 const graph = new GraphLA('A');
-let indexB = graph.addVertice('B');
-let indexC = graph.addVertice('C');
-let indexD = graph.addVertice('D');
-graph.addEdges(0,{destino:1,peso:3});
-graph.addEdges(0,{destino:2,peso:2});
-graph.addEdges(indexC,{destino:1,peso:1});
-graph.addEdges(indexD,{destino:0,peso:4});
+let A = 0;
+let C = graph.addVertice('C');
+let F = graph.addVertice('F');
+let G = graph.addVertice('G');
+let E = graph.addVertice('E');
+let B = graph.addVertice('B');
+let D = graph.addVertice('D');
 
-console.log(graph.DFS(0));
-console.log(graph.DFS(indexD));
+
+
+graph.addEdges(A,{destino:C,peso:3});
+
+graph.addEdges(C,{destino:F,peso:2});
+graph.addEdges(C,{destino:G,peso:4});
+graph.addEdges(C,{destino:E,peso:1});
+
+graph.addEdges(F,{destino:B,peso:2});
+graph.addEdges(B,{destino:C,peso:5});
+
+graph.addEdges(E,{destino:A,peso:1});
+
+graph.addEdges(D,{destino:A,peso:2});
+graph.addEdges(D,{destino:E,peso:1});
+
+//graph.PrintData();
+
+console.log(graph.DFS(D));
+console.log(graph.BFS(D));
